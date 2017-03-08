b6686db90dba7350cc8b738202929d94cb01fe91
Damon <damon@mahuika.co.nz>
2017-03-08 22:37:10 +0700

    Don't rely on global setup of shelljs

--- src/getDevName.js
+++ src/getDevName.js
@@ -1,8 +1,13 @@
 import shell from 'shelljs'
 
 
-export default () =>
-  shell.exec('git config user.name', { timeout: 1000 })
+export default () => {
+  const result = shell.exec('git config user.name', { timeout: 1000, silent: true })
+  if (result.code !== 0) {
+    throw new Error('Failed to execute git')
+  }
+  return result
     .stdout
     .replace('\n', '')
     .toLowerCase()
+}
