edfaf9e2cf827644cd765451b5f95a4d445b52f1
Damon <damon@mahuika.co.nz>
2017-04-18 17:28:22 +0700

    deployMeteor.js

---
+++ src/deployMeteor.js
@@ -0,0 +1,25 @@
+import gutil from 'gulp-util'
+import streamToPromise from 'stream-to-promise'
+
+
+export default async ({
+  host,
+  meteorRootPath,
+  galaxyRegion,
+  meteorSettingsPath,
+}) => {
+  const galaxyManagementServer = `${galaxyRegion}.galaxy.meteor.com`
+  await streamToPromise(
+    run(`meteor deploy ${host} --settings ${meteorSettingsPath}`, {
+      env: {
+        ...process.env,
+        DEPLOY_HOSTNAME: galaxyManagementServer,
+      },
+      cwd: meteorRootPath,
+    }).exec()
+  )
+  gutil.log(
+    `Check status of Mete${gutil.colors.red('o')}r deployment at`,
+    gutil.colors.blue(`https://${galaxyManagementServer}/app/${host}/logs`),
+  )
+}
