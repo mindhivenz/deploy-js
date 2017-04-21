9d43ae3c4c2b6b948d96a8d9d2e2625a170df086
Damon <damon@mahuika.co.nz>
2017-04-21 19:46:48 +0700

    Don't split params across multiple lines

--- src/serverlessParams.js
+++ src/serverlessParams.js
@@ -4,5 +4,5 @@ import { awsCredentialsProfile } from './awsServiceOptions'
 
 export default ({ proj, stage }) =>
   // use '--stageLocal' so doesn't interfere with serverless option '--stage' which should have the value of stagePublic
-  `--stageLocal ${stage} --stagePublic ${publicStageName(stage)} 
-  --credentialsProfile ${awsCredentialsProfile({ proj, stage })}`
+  `--stageLocal ${stage} --stagePublic ${publicStageName(stage)} ` +
+  `--credentialsProfile ${awsCredentialsProfile({ proj, stage })}`
