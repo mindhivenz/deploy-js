6a8edc305cb5dfa80db20c986a745083e03c22be
Damon <damon@mahuika.co.nz>
2017-05-11 14:11:09 +0700

    Don't put out --credentialsProfile if none

--- src/serverlessParams.js
+++ src/serverlessParams.js
@@ -2,7 +2,12 @@ import publicStageName from './publicStageName'
 import { awsCredentialsProfile } from './awsServiceOptions'
 
 
-export default ({ proj, stage }) =>
+export default ({ proj, stage }) => {
   // use '--stageLocal' so doesn't interfere with serverless option '--stage' which should have the value of stagePublic
-  `--stageLocal ${stage} --stagePublic ${publicStageName(stage)} ` +
-  `--credentialsProfile ${awsCredentialsProfile({ proj, stage })}`
+  const credentialsProfile = awsCredentialsProfile({ proj, stage })
+  return [
+    `--stageLocal ${stage}`,
+    `--stagePublic ${publicStageName(stage)}`,
+    ...(credentialsProfile ? [`--credentialsProfile ${credentialsProfile}`] : []),
+  ].join(' ')
+}
