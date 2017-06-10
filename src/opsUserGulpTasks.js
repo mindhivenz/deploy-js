f445e8f7de5fbf3e37e516b461d66e0e279a8894
Damon <damon@mahuika.co.nz>
2017-06-10 14:09:18 +0800

    Cleanup of opsUserGulpTasks

--- src/opsUserGulpTasks.js
+++ src/opsUserGulpTasks.js
@@ -2,10 +2,10 @@ import gulp from 'gulp'
 import gutil from 'gulp-util'
 import IAM from 'aws-sdk/clients/iam'
 import KMS from 'aws-sdk/clients/kms'
-import uniq from 'lodash/uniq'
 import request from 'request-promise-native'
 import querystring from 'querystring'
 import opn from 'opn'
+import yargs from 'yargs'
 
 import devName, { requireDevNameSpecified } from './devName'
 import publicStageName from './publicStageName'
@@ -16,81 +16,91 @@ import { proj as credentialsFactory } from './awsCredentials'
 
 const masterAccountId = '087916394902'
 const deploySecretsKeyId = `arn:aws:kms:us-east-1:${masterAccountId}:key/9cef9467-af1d-45e6-bb16-030d8c1ec237`
-const deploySecretsKeyOperations = ['Decrypt', 'GenerateDataKey']
+const opsUserName = () => {
+  requireDevNameSpecified()
+  return `${devName()}.ops`
+}
 
-const copyColor = gutil.colors.yellow
-
-const opsUserName = () => `${devName()}.ops`
+const iamFactory = () => new IAM({ apiVersion: '2010-05-08', ...awsOpts })
+const kmsFactory = () => new KMS({ apiVersion: '2014-11-01', ...awsOpts })
 
 const opsUserArn = async () => {
-  const iam = new IAM({ apiVersion: '2010-05-08', ...awsOpts })
+  const iam = iamFactory()
   const getUserResult = await iam.getUser({
     UserName: opsUserName(),
   }).promise()
   return getUserResult.User.Arn
 }
 
+const grantSecrets = async (userArn, grants) => {
+  const kms = kmsFactory()
+  await Promise.all(grants.map(({ proj, stage }) => {
+    if (! proj && stage !== publicStageName('dev')) {
+      throw new Error("Can only grant organization-wide to a user's own dev stage")
+    }
+    const Name = proj ? `${proj}/${stage || 'any'}/${devName()}` : stage
+    return kms.createGrant({
+      KeyId: deploySecretsKeyId,
+      GranteePrincipal: userArn,
+      Name,
+      Constraints: { EncryptionContextSubset: { proj, stage } },
+      Operations: ['Decrypt', 'GenerateDataKey'],
+    }).promise()
+  }))
+}
+
+const copyColor = gutil.colors.yellow
+
 export default (proj, stages) => {
 
-  gulp.task('who-am-i', async () => gutil.log(`devName: ${copyColor(devName())}`))
+  gulp.task('who-am-i', async () => gutil.log(`You are devName: ${copyColor(devName())}`))
 
   gulp.task('add:ops-user', async () => {
-    requireDevNameSpecified()
-    const UserName = opsUserName()
+    yargs.usage('Add a new ops user')
+    const UserName = opsUserName()  // Perform this early to catch required param
     const devPublicStage = publicStageName('dev')
-    const iam = new IAM({ apiVersion: '2010-05-08', ...awsOpts })
-    const kms = new KMS({ apiVersion: '2014-11-01', ...awsOpts })
-    const createUserResult = await iam.createUser({
-      UserName,
-      Path: '/ops/',
-    }).promise()
-    await iam.addUserToGroup({
-      UserName,
-      GroupName: 'ops',
-    }).promise()
-    await kms.createGrant({
-      KeyId: deploySecretsKeyId,
-      GranteePrincipal: createUserResult.User.Arn,
-      Name: devPublicStage,
-      Constraints: { EncryptionContextSubset: { stage: devPublicStage } },
-      Operations: deploySecretsKeyOperations,
-    }).promise()
-    const createAccessKeyResult = await iam.createAccessKey({
-      UserName,
-    }).promise()
+    const iam = iamFactory()
+    const createUserResult = await iam.createUser({ UserName, Path: '/ops/' }).promise()
+    await iam.addUserToGroup({ UserName, GroupName: 'ops' }).promise()
+    await grantSecrets(createUserResult.User.Arn, [{ stage: devPublicStage }])
+    const createAccessKeyResult = await iam.createAccessKey({ UserName }).promise()
     const accessKey = createAccessKeyResult.AccessKey
-    gutil.log('Copy the SecretAccessKey now, it cannot be retrieved again')
-    gutil.log(`UserName: ${UserName}`)
-    gutil.log(`AccessKeyId: ${copyColor(accessKey.AccessKeyId)}`)
-    gutil.log(`SecretAccessKey: ${copyColor(accessKey.SecretAccessKey)}`)
-    gutil.log(`Get user to save the keys using: ${gutil.colors.dim('aws-keychain add mindhive-ops')}`)
-    gutil.log(`They have already been granted access to secrets for the ${devPublicStage} stage in all projects`)
+    gutil.log([
+      'Copy the SecretAccessKey now, it cannot be retrieved again',
+      `UserName: ${UserName}`,
+      `AccessKeyId: ${copyColor(accessKey.AccessKeyId)}`,
+      `SecretAccessKey: ${copyColor(accessKey.SecretAccessKey)}`,
+      `Get user to save the keys using: ${gutil.colors.dim('aws-keychain add mindhive-ops')}`,
+      `They have been granted access to secrets in the ${devPublicStage} stage of any project`,
+    ].join('\n'))
+  })
+
+  gulp.task('grant:ops-user:secrets', async () => {
+    yargs.usage(`Grant an ops user access to ${proj} secrets in any stage`)
+    await grantSecrets(await opsUserArn(), [{ proj }])
   })
 
   stages.forEach((stage) => {
 
     gulp.task(`grant:ops-user:secrets:${stage}`, async () => {
-      requireDevNameSpecified()
-      const kms = new KMS({ apiVersion: '2014-11-01', ...awsOpts })
-      const userArn = await opsUserArn()
-      await Promise.all(uniq([publicStageName(stage), allStages]).map(s =>
-        kms.createGrant({
-          KeyId: deploySecretsKeyId,
-          GranteePrincipal: userArn,
-          Name: [proj, s, devName()].join('/'),
-          Constraints: { EncryptionContextSubset: { proj, stage: s } },
-          Operations: deploySecretsKeyOperations,
-        }).promise()
-      ))
+      yargs.usage(`Grant an ops user access to ${proj} ${stage} secrets`)
+      const userArn = await opsUserArn()  // Perform this early to catch required param
+      await grantSecrets(
+        userArn,
+        [
+          { proj, stage: publicStageName(stage) },
+          { proj, stage: allStages },
+        ],
+      )
     })
 
     gulp.task(`grant:ops-user:access:${stage}`, async () => {
-      requireDevNameSpecified()
-      const iam = new IAM({ apiVersion: '2010-05-08', ...awsOpts })
       const account = await resolve({ proj, stage })
+      yargs.usage(`Grant an ops user full access to the ${account.name} AWS account`)
       const UserName = opsUserName()  // Perform this early to catch required param
       const PolicyName = `FullAccess@${account.Name}`
       const PolicyArn = `arn:aws:iam::${masterAccountId}:policy/${PolicyName}`
+      const iam = iamFactory()
       try {
         await iam.getPolicy({ PolicyArn }).promise()
       } catch (e) {
@@ -109,13 +119,6 @@ export default (proj, stages) => {
       }
       await iam.attachUserPolicy({ PolicyArn, UserName }).promise()
     })
-
-    gulp.task(`grant:ops-user:${stage}`,
-      gulp.parallel(
-        `grant:ops-user:secrets:${stage}`,
-        `grant:ops-user:access:${stage}`,
-      ),
-    )
 
     gulp.task(`open:aws:${stage}`, async () => {
       const credentials = credentialsFactory({ proj, stage })
