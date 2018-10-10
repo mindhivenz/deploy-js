a3086f21f248debd58044af6d2202d6d0ec0d927
damon <damon@mahuika.co.nz>
2018-10-10 10:41:43 +0600

    Buffer jsonSrc

--- src/jsonSrc.js
+++ src/jsonSrc.js
@@ -1,9 +1,10 @@
 import source from 'vinyl-source-stream'
 import intoStream from 'into-stream'
+import buffer from 'vinyl-buffer'
 
 import jsonPretty from './jsonPretty'
-
 
 export default (filename, obj) =>
   intoStream(jsonPretty(obj))
     .pipe(source(filename))
+    .pipe(buffer())
