f01b9f515e11a3d43a520d0805349721ccdafcf9
damon <damon@mahuika.co.nz>
2019-03-19 22:00:36 +1300

    Rename import

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,7 +2,7 @@ import del from 'del'
 import gulp from 'gulp'
 import ensureGitUpToDate from './src/ensureGitUpToDate'
 import execFile from './src/execFile'
-import yarnPublish from './src/yarnPublishDist'
+import yarnPublishDist from './src/yarnPublishDist'
 
 const distDir = 'dist'
 
@@ -23,6 +23,6 @@ export const dist = gulp.series(clean, gulp.parallel(build, copy))
 const gitUpToDate = () => ensureGitUpToDate('.')
 
 const publish = () =>
-  yarnPublish({ srcPackageDir: '.', distPackageDir: distDir })
+  yarnPublishDist({ srcPackageDir: '.', distPackageDir: distDir })
 
 export const release = gulp.series(gitUpToDate, dist, publish)
