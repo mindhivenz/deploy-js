2f01c1d759fb33fcd22be6855a15ce8363d3aae6
damon <damon@mahuika.co.nz>
2019-09-27 16:07:11 +1200

    Enforce string result from awsAccountId.ts

--- src/awsAccountId.ts
+++ src/awsAccountId.ts
@@ -2,10 +2,13 @@ import STS from 'aws-sdk/clients/sts'
 import memoize from 'lodash/memoize'
 import { IOptions, projCredentialsFactory } from './internal/awsProjCredentials'
 
-const getAccountId = async (options: IOptions) => {
+const getAccountId = async (options: IOptions): Promise<string> => {
   const credentials = projCredentialsFactory(options)
   const sts = new STS({ credentials })
   const result = await sts.getCallerIdentity({}).promise()
+  if (!result.Account) {
+    throw new Error(`Unexpected no account returned`)
+  }
   return result.Account
 }
 
