59a4867758e9f8cf339713a3aad79fa2313888b4
damon <damon@mahuika.co.nz>
2019-09-30 10:22:54 +1300

    Add AWS_DEFAULT_REGION to awsCredentialsEnv.ts

--- src/awsCredentialsEnv.ts
+++ src/awsCredentialsEnv.ts
@@ -1,11 +1,19 @@
-import { IOptions, projCredentialsFactory } from './internal/awsProjCredentials'
+import {
+  IOptions as IProjOptions,
+  projCredentialsFactory,
+} from './internal/awsProjCredentials'
 
-export default async (options: IOptions) => {
-  const credentials = projCredentialsFactory(options)
+interface IOptions extends IProjOptions {
+  region?: string
+}
+
+export default async ({ region, ...projOptions }: IOptions) => {
+  const credentials = projCredentialsFactory(projOptions)
   await credentials.getPromise()
   return {
     AWS_ACCESS_KEY_ID: credentials.accessKeyId,
     AWS_SECRET_ACCESS_KEY: credentials.secretAccessKey,
     AWS_SESSION_TOKEN: credentials.sessionToken,
+    ...(region ? { AWS_DEFAULT_REGION: region } : {}),
   }
 }
