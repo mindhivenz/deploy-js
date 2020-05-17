1668d80cb4fc57032b52dbe4452a3e7a479f44b6
damon <damon@mahuika.co.nz>
2020-05-18 11:33:12 +1200

    Show global yargs options in their own group

--- src/internal/args.ts
+++ src/internal/args.ts
@@ -1,9 +1,13 @@
 import yargs from 'yargs'
 
+const group = 'Global options'
+
 export const globalArgs = yargs
   .option('verbose', {
     describe: 'Stream all sub-process output to console',
+    group,
   })
   .option('devName', {
     describe: "Override your 'dev' name, defaults to your git username",
+    group,
   })
