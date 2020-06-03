fded57993a0d917b26e33250cb1ab84252191a44
damon <damon@mahuika.co.nz>
2020-06-03 13:09:40 +1200

    Alter bumpVersion args

--- src/bumpVersion.ts
+++ src/bumpVersion.ts
@@ -1,14 +1,15 @@
 import log from 'fancy-log'
 import path from 'path'
 import PluginError from 'plugin-error'
-import shell, { ExecOutputReturnValue } from 'shelljs'
 
 import ensureGitUpToDate from './ensureGitUpToDate'
+import execFile from './execFile'
 import { globalArgs } from './internal/args'
 import { highlight } from './internal/colors'
 import readJson from './readJson'
 
 const pluginName = '@mindhive/deploy/bumpVersion'
+const group = 'Version bump'
 
 export default async (packageJsonPath: string = './package.json') => {
   const readPackageVersion = (): string => {
@@ -23,27 +24,35 @@ export default async (packageJsonPath: string = './package.json') => {
     }
   }
 
-  const versionBump = globalArgs.option('version-bump', {
-    default: 'patch',
-    describe:
-      "'patch', 'minor', 'major', a literal version number, or 'same' to not change version",
-  }).argv['version-bump']
+  const args = globalArgs
+    .option('patch', {
+      boolean: true,
+      default: true,
+      group,
+    })
+    .option('minor', {
+      boolean: true,
+      group,
+    })
+    .option('major', {
+      boolean: true,
+      conflicts: ['minor'],
+      group,
+    })
+    .option('same', {
+      boolean: true,
+      conflicts: ['minor', 'major'],
+      group,
+    }).argv
+  const yarnArg = args.major ? '--major' : args.minor ? '--minor' : '--patch'
   const cwd = path.dirname(packageJsonPath)
   await ensureGitUpToDate(cwd, { pluginName })
-  const currentVersion = readPackageVersion()
-  if (versionBump === 'same') {
-    return currentVersion
+  if (args.same) {
+    return readPackageVersion()
   }
-  const yarnArgs = ['major', 'minor', 'patch'].includes(versionBump)
-    ? `--${versionBump}`
-    : `--new-version ${versionBump}`
-  const bumpResult = shell.exec(`yarn version ${yarnArgs}`, {
+  await execFile('yarn', ['version', yarnArg], {
     cwd,
-    silent: true,
-  }) as ExecOutputReturnValue
-  if (bumpResult.code !== 0) {
-    throw new PluginError(pluginName, bumpResult.stderr)
-  }
+  })
   const newVersion = readPackageVersion()
   log(`Version bumped to: ${highlight(newVersion)}`)
   return newVersion
--- src/internal/args.ts
+++ src/internal/args.ts
@@ -3,11 +3,14 @@ import yargs from 'yargs'
 const group = 'Global options'
 
 export const globalArgs = yargs
+  .version(false)
   .option('verbose', {
     describe: 'Stream all sub-process output to console',
     group,
+    string: true,
   })
   .option('devName', {
-    describe: "Override your 'dev' name, defaults to your git username",
+    describe: "Override your 'dev' name",
     group,
+    defaultDescription: 'your git username',
   })
