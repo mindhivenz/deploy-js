4ffac24643002d1bb61d6d1401edf74e56f5c5c7
damon <damon@mahuika.co.nz>
2020-07-04 14:30:25 +1200

    Rename DatadogEc2InstancePolicyStatement.ts sid

--- src/cdk-constructs/DatadogEc2InstancePolicyStatement.ts
+++ src/cdk-constructs/DatadogEc2InstancePolicyStatement.ts
@@ -3,7 +3,7 @@ import { PolicyStatement } from '@aws-cdk/aws-iam'
 export class DatadogEc2InstancePolicyStatement extends PolicyStatement {
   constructor() {
     super({
-      sid: 'datadog-ec2-access',
+      sid: 'DatadogEc2Access',
       actions: ['ec2:Describe*', 'ec2:Get*'],
       resources: ['*'],
     })
