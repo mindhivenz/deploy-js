5629c8371713f00fa31e989b7395bae8c7bbd983
damon <damon@mahuika.co.nz>
2020-09-13 09:32:04 +1200

    Allow ignoring of git state in ensureGitUpToDate.ts

--- src/ensureGitUpToDate.ts
+++ src/ensureGitUpToDate.ts
@@ -1,5 +1,7 @@
 import PluginError from 'plugin-error'
+import log from 'fancy-log'
 import execFile from './execFile'
+import { globalArgs } from './internal/args'
 
 // Based off: https://stackoverflow.com/a/3278427/3424884
 
@@ -10,7 +12,14 @@ interface IOptions {
 export default async (
   repoPath?: string,
   { pluginName = '@mindhive/deploy/ensureGitUpToDate' }: IOptions = {},
-) => {
+): Promise<string> => {
+  const argv = globalArgs.option('ignore-git', {
+    boolean: true,
+  }).argv
+  if (argv['ignore-git']) {
+    log('Ignoring git state')
+    return 'Ignoring git state'
+  }
   const options = { cwd: repoPath, captureOutput: true }
   try {
     await execFile(
@@ -31,6 +40,7 @@ export default async (
   if (local === base) {
     throw new PluginError(pluginName, 'You are behind origin: pull')
   } else if (remote === base) {
+    log('Ahead of origin, need to push')
     return 'Ahead of origin, need to push'
   } else {
     throw new PluginError(
