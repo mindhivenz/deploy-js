aeb47bd54272aba99cb85ed5ca5382f73c447366
damon <damon@mahuika.co.nz>
2021-04-03 20:03:57 +1300

    Fix resolve return

--- src/ensureNodeModulesDeterministic.ts
+++ src/ensureNodeModulesDeterministic.ts
@@ -20,7 +20,7 @@ export default async (path: string) => {
               ),
             )
           } else {
-            resolve()
+            resolve(undefined)
           }
         },
       )
