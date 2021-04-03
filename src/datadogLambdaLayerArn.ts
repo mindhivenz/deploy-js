dc134cd3b4231db9f17efd468ed7d5405408d0c4
damon <damon@mahuika.co.nz>
2021-04-03 20:02:12 +1300

    Updated datadogLambdaLayerArn.ts

--- src/datadogLambdaLayerArn.ts
+++ src/datadogLambdaLayerArn.ts
@@ -2,7 +2,9 @@ import { Runtime } from '@aws-cdk/aws-lambda'
 
 export default (region: string, runtime: Runtime): string => {
   if (runtime.runtimeEquals(Runtime.NODEJS_12_X)) {
-    return `arn:aws:lambda:${region}:464622532012:layer:Datadog-Node12-x:24`
+    return `arn:aws:lambda:${region}:464622532012:layer:Datadog-Datadog-Node12-x:51`
+  } else if (runtime.runtimeEquals(Runtime.NODEJS_14_X)) {
+    return `arn:aws:lambda:${region}:464622532012:layer:Datadog-Datadog-Node14-x:51`
   }
   throw new Error(`No layer set for runtime ${runtime}`)
 }
