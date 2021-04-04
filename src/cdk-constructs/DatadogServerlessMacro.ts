fdb7b1e424dbdb59baec47d4bf3832f1797ef6b1
damon <damon@mahuika.co.nz>
2021-04-04 18:26:15 +1200

    DatadogServerlessMacro

---
+++ src/cdk-constructs/DatadogServerlessMacro.ts
@@ -0,0 +1,38 @@
+/* tslint:disable no-unused-expression */
+import { Function as LambdaFunction } from '@aws-cdk/aws-lambda'
+import { CfnSubscriptionFilter } from '@aws-cdk/aws-logs'
+import { CfnMapping, Construct, Stack } from '@aws-cdk/core'
+import importDatadogForwarderArn from './importDatadogForwarderArn'
+
+interface DatadogServerlessProps {
+  logLevel?: 'INFO' | 'DEBUG'
+  service?: string
+  env?: string
+}
+
+class DatadogServerlessMacro extends Construct {
+  constructor(stack: Stack, id: string, props: DatadogServerlessProps = {}) {
+    super(stack, id)
+    new CfnMapping(stack, 'Datadog', {
+      mapping: {
+        Parameters: {
+          nodeLayerVersion: '51',
+          pythonLayerVersion: '32',
+          stackName: stack.stackName,
+          service: stack.stackName,
+          logLevel: 'INFO',
+          ...props,
+        },
+      },
+    })
+    stack.addTransform('DatadogServerless')
+  }
+
+  monitorFunction(func: LambdaFunction) {
+    new CfnSubscriptionFilter(this, `${func.node.id}-DatadogSubscription`, {
+      filterPattern: '',
+      logGroupName: func.logGroup.logGroupName,
+      destinationArn: importDatadogForwarderArn(),
+    })
+  }
+}
