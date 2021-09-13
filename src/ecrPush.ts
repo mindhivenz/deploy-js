3c00a4e24bce7a58eeccfccde7f05881e5ccd2ee
damon <damon@mahuika.co.nz>
2021-09-13 15:50:53 +1200

    Option to tagWithGitHash in ecrPush

--- src/ecrPush.ts
+++ src/ecrPush.ts
@@ -3,6 +3,7 @@ import { highlight } from './colors'
 import awsCredentialsEnv from './awsCredentialsEnv'
 import ecrImageRepo from './ecrImageRepo'
 import execFile from './execFile'
+import gitHash from './gitHash'
 import { IOptions as IEcrOptions } from './internal/ecr'
 import setEcrCredentialHelper from './setEcrCredentialHelper'
 
@@ -10,25 +11,45 @@ interface IOptions extends IEcrOptions {
   localImageTag: string
   repoName: string
   remoteImageTag?: string
+  tagWithGitHash?: boolean
+  gitRepoPath?: string
 }
 
 export default async ({
   localImageTag,
   repoName,
   remoteImageTag,
+  tagWithGitHash = false,
+  gitRepoPath,
   ...ecrOptions
 }: IOptions) => {
   const repo = await ecrImageRepo({ name: repoName, ...ecrOptions })
-  const ecrImageTag = remoteImageTag ? `${repo}:${remoteImageTag}` : repo
   await setEcrCredentialHelper(ecrOptions)
-  await execFile('docker', ['tag', localImageTag, ecrImageTag])
+  const tags = remoteImageTag ? [remoteImageTag] : []
+  if (tagWithGitHash) {
+    const hash = await gitHash(gitRepoPath, { gitUpToDate: true })
+    tags.push(hash)
+  } else {
+    if (tags.length === 0) {
+      tags.push('latest')
+    }
+  }
+  for (const tag of tags) {
+    await execFile('docker', ['tag', localImageTag, `${repo}:${tag}`])
+  }
   const awsEnv = await awsCredentialsEnv(ecrOptions)
-  await execFile('docker', ['push', ecrImageTag], {
-    env: {
-      ...process.env,
-      ...awsEnv,
-    },
-    pipeOutput: true,
-  })
-  log(`Pushed ${highlight(localImageTag)} -> ${highlight(ecrImageTag)}`)
+  for (const tag of tags) {
+    await execFile('docker', ['push', `${repo}:${tag}`], {
+      env: {
+        ...process.env,
+        ...awsEnv,
+      },
+      pipeOutput: true,
+    })
+  }
+  log(
+    `Pushed ${highlight(localImageTag)} -> ${highlight(
+      `${repo}:${tags.join(',')}`,
+    )}`,
+  )
 }
