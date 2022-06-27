bfb15b2c324f45ca729166a258be7b05f23edc23
damon <damon@mahuika.co.nz>
2022-06-27 17:44:59 +1200

    revert: If --ignore-git then don't gitPush

--- src/gitPush.ts
+++ src/gitPush.ts
@@ -1,15 +1,6 @@
-import log from 'fancy-log'
 import execFile from './execFile'
-import { globalArgs } from './internal/args'
 
 export default async (repoPath?: string, remote: string = 'origin') => {
-  const { ignoreGit } = await globalArgs.option('ignore-git', {
-    boolean: true,
-  }).argv
-  if (ignoreGit) {
-    log('Skipping push')
-    return
-  }
   await execFile('git', ['push', '--follow-tags', remote], {
     cwd: repoPath,
   })
