da66c87a02a055abce1714d8306a7372941bc4c8
tim <tim@mindhive.co.nz>
2025-04-01 15:06:30 +1300

    do some git checking foo to get correct branch

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -18,7 +18,33 @@ else
   release_branch=release/${current_branch}
 fi
 
-git checkout -b ${release_branch}
+# Checkout the release branch
+if git show-ref --verify --quiet refs/heads/$release_branch; then
+  echo "Checking out existing $release_branch."
+  git checkout $release_branch
+else
+  echo "$release_branch does not exist. Checking if release exists."
+
+  # Ensure that the base branch release exists locally
+  if git show-ref --verify --quiet refs/heads/release; then
+    echo "Base branch release found locally."
+  else
+    echo "release not found locally. Trying to fetch from remote."
+    if git show-ref --verify --quiet refs/remotes/origin/release; then
+      echo "Fetching release from remote."
+      git fetch origin release:release
+    else
+      echo "Error: Base branch release not found locally or on remote."
+      exit 1
+    fi
+  fi
+
+  # Create and switch to release/test from release
+  echo "Creating and checking out $release_branch from release."
+  git checkout -b $release_branch release
+fi
+
+exit 1
 
 # Make dist
 find . -mindepth 1 -maxdepth 1 ! -name 'dist' ! -name '.gitignore' ! -name 'node_modules' ! -name 'deploy/node_modules' ! -name '.git' -exec rm -rf {} +
@@ -29,8 +55,8 @@ git add -A
 git commit -m "Release"
 
 if [ "$push" = true ]; then
-  echo "Pushing to origin/$release_branch"
-  git push origin/$release_branch
+  echo "Pushing to origin/$$release_branch"
+  git push origin/$$release_branch
 else
-  echo "Would push to origin/$release_branch"
+  echo "Would push to origin/$$release_branch"
 fi
